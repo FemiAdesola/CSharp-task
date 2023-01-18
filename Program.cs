5df72c845c26b7ba73c5086089b106816bfa9aed
Femi Adesola Oyinloye <75606768+FemiAdesola@users.noreply.github.com>
2023-01-18 14:45:36 +0200

    done task 2

--- Program.cs
+++ Program.cs
@@ -12,11 +12,32 @@ namespace CSharpFundamental
             string newText = toTitleCase(text);
             Console.WriteLine(newText); // expect to see "This Is A Text"
 
-            // /* Challenge 2 */
-            // int[,] arrayA = { { 3, 5, 4, 6 }, { 3, 7, 8, 3 } };
-            // int[,] arrayB = { { 5, 1 }, { 8, 4 }, { 2, 9 }, { 2, 3 } };
-            // int[,] result = matrixMultiply(arrayA, arrayB);
-            // Console.WriteLine(result); //share your findings to Slack
+            /* Challenge 2 */
+            int[,] arrayA = { 
+                { 3, 5, 4, 6 }, 
+                { 3, 7, 8, 3 } 
+                }; // 4D2
+            int[,] arrayB = { 
+                { 5, 1 }, 
+                { 8, 4 }, 
+                { 2, 9 }, 
+                { 2, 3 } 
+                }; // 2D4
+            int[,] result = matrixMultiply(arrayA, arrayB);
+            Console.WriteLine(result); //share your findings to Slack
+            {
+                int arrayARows = result.GetLength(0);
+                int arrayBCols = result.GetLength(1);
+
+                for (int i = 0; i < arrayARows; i++)
+                {
+                    for (int j = 0; j < arrayBCols; j++)
+                    {
+                        Console.Write($" {result[i, j]} ");
+                    }
+                    Console.Write(Environment.NewLine);
+                }
+            }
         }
         static string toTitleCase(string input)
         {
@@ -55,10 +76,31 @@ namespace CSharpFundamental
             }
             return stringText.Trim();
         }
-        
-        // static int[,] matrixMultiply(int[,] array1, int[,] array2)
-        // {
-        //     /* Write your code here */
-        // }
+
+        static int[,] matrixMultiply(int[,] array1, int[,] array2)
+        {
+            /* Write your code here */
+            var array1Rows = array1.GetLength(0);
+            var array1Cols = array1.GetLength(1);
+            var array2Rows = array2.GetLength(0);
+            var array2Cols = array2.GetLength(1);
+
+            if (array1Cols != array2Rows)
+            {
+                throw new ArgumentException("This array must have the same length");
+            }
+           int[,] result = new int[array1Rows, array2Cols];
+            for (int i = 0; i < array1Rows; i++)
+            {
+                for (int j = 0; j < array1Cols; j++)
+                {
+                    for (int k = 0; k < array2Cols; k++)
+                    {
+                        result[i, k] += array1[i, j] * array2[j, k];
+                    }
+                }
+            }
+            return result;
+        }
     }
 }
